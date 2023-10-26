bf20a415c45e6c22b1a163b2b6428bee0086509a
Sergiyan92 <sergiyan@ukr.net>
2023-10-26 11:12:23 +0300

    style feedback

--- src/components/Viget.tsx
+++ src/components/Viget.tsx
@@ -1,49 +1,4 @@
-// import React, { useState } from "react";
-
-// interface VigetProps {
-//   good: number;
-//   neutral: number;
-//   bad: number;
-// }
-
-// const Viget = () => {
-//   const [good, setGood] = useState(0);
-//   const [neutral, setNeutral] = useState(0);
-//   const [bad, setBad] = useState(0);
-//   const [total, setTotal] = useState(0);
-//   const [positive, setPositive] = useState(0);
-
-//   const handleFeedback = (category: "good" | "neutral" | "bad") => {
-//     category + 1;
-//   };
-//   const TotalFeedback = () => {
-//     return good + neutral + bad;
-//   };
-//   const PositiveFeedbackPercentage = () => {
-//     return setPositive(
-//       TotalFeedback() > 0 ? Math.round((good / TotalFeedback) * 100) : 0
-//     );
-//   };
-//   const hasFeedback = TotalFeedback() > 0;
-//   return (
-//     <div>
-//       <p>Good: {good}</p>
-//       <p>Neutral: {neutral}</p>
-//       <p>Bad: {bad}</p>
-//       <p>Total: {total}</p>
-//       <p>Positive feedback: {positive}%</p>
-//     </div>
-//   );
-// };
-
-// export default Viget;
 import { useState } from "react";
-
-// interface VigetProps {
-//   good: number;
-//   neutral: number;
-//   bad: number;
-// }
 
 const Viget = () => {
   const [good, setGood] = useState(0);
@@ -65,11 +20,28 @@ const Viget = () => {
   const hasFeedback = total > 0;
 
   return (
-    <>
-      <h2>Statistics</h2>
-      <button onClick={() => handleFeedback("good")}>Good</button>
-      <button onClick={() => handleFeedback("neutral")}>Neutral</button>
-      <button onClick={() => handleFeedback("bad")}>Bad</button>
+    <div className="container w-[300px] m-0 ml-auto mr-auto border p-5 mt-10">
+      <h2 className=" text-2xl font-bold text-center mb-4">Feedback</h2>
+      <div className=" flex justify-around mb-4 ">
+        <button
+          className="border rounded p-1"
+          onClick={() => handleFeedback("good")}
+        >
+          Good
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => handleFeedback("neutral")}
+        >
+          Neutral
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => handleFeedback("bad")}
+        >
+          Bad
+        </button>
+      </div>
       {hasFeedback ? (
         <div>
           <p>Good: {good}</p>
@@ -79,9 +51,9 @@ const Viget = () => {
           <p>Positive feedback: {positive}%</p>
         </div>
       ) : (
-        <h2>There is no feedback</h2>
+        <h2 className=" text-center">There is no feedback</h2>
       )}
-    </>
+    </div>
   );
 };
 
