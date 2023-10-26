aeca0a3b6eb88f3f4af6166db80c8af93de78b56
Sergiyan92 <sergiyan@ukr.net>
2023-10-26 16:28:57 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
-    <nav className="h-[50px] flex justify-between px-5 bg-gray-500 items-center text-white">
+    <nav className=" sticky top-0 h-[50px]  flex justify-between px-5 bg-gray-500 items-center text-white">
       <span className="font-bold">React-Typescript</span>
       <span>
         <Link to="/" className="mr-2">
