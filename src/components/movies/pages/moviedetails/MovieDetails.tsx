3f2cfa513b68b9900bbcab648fa4516a64b28b61
Sergiyan92 <sergiyan@ukr.net>
2023-10-31 11:40:24 +0200

    Update MovieDetails.tsx

--- src/components/movies/pages/moviedetails/MovieDetails.tsx
+++ src/components/movies/pages/moviedetails/MovieDetails.tsx
@@ -1,5 +1,48 @@
-const MovieDetails = () => {
-  return <div>MovieDetails</div>;
+import CardItem from "../../components/card/CardItem";
+import { IfoItem } from "../../components/info/InfoItem";
+import { useEffect, useRef, useState } from "react";
+
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
+import { getMovieId } from "../../../../service/service";
+type MovieDetails = {
+  backdrop_path: string | null;
+  original_title: string;
+  overview: string;
+  genres: Genre[];
 };
 
-export default MovieDetails;
+type Genre = {
+  id: number;
+  name: string;
+};
+
+const MoviesDetails = () => {
+  const { movieId } = useParams<{ movieId: string }>();
+  const [details, setDetails] = useState<MovieDetails | null>(null);
+
+  const location = useLocation();
+  const back = useRef(location.state ?? "/");
+
+  useEffect(() => {
+    if (movieId) {
+      const id = parseInt(movieId);
+      getMovieId(id)
+        .then((data) => setDetails(data))
+        .catch((error) => console.log(error));
+    }
+  }, [movieId]);
+
+  if (!details) return;
+  return (
+    <div>
+      <Link className="" to={back.current}>
+        Back
+      </Link>
+      <CardItem card={details} />
+      <IfoItem />
+
+      <Outlet />
+    </div>
+  );
+};
+export default MoviesDetails;
