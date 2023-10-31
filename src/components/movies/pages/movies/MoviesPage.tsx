0977a219449cabb316982cf2bfe3472549c7e43e
Sergiyan92 <sergiyan@ukr.net>
2023-10-31 11:46:45 +0200

    Update MoviesPage.tsx

--- src/components/movies/pages/movies/MoviesPage.tsx
+++ src/components/movies/pages/movies/MoviesPage.tsx
@@ -1,5 +1,56 @@
-const MoviesPage = () => {
-  return <div>MoviesPage</div>;
-};
+import Searchbar from "../../components/searchform/SearchForm";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+import { getMovies } from "../../../../service/service";
+interface Movie {
+  id: number;
+  title: string;
+}
+const Movies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [error, setMoviesError] = useState<string | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query");
+  const location = useLocation();
+  useEffect(() => {
+    if (!query) return;
+    try {
+      const fetchMovies = async () => {
+        const { movieData } = await getMovies(query);
+        setMovies(() => [...movieData]);
+      };
+      fetchMovies();
+    } catch (error) {
+      if (typeof error === "string") {
+        setMoviesError(error);
+      } else {
+        setMoviesError("An error occurred");
+      }
+    }
+  }, [query]);
 
-export default MoviesPage;
+  const handleSubmit = (query: string) => {
+    if (query === "") {
+      alert("Please enter a query");
+    } else {
+      setSearchParams({ query });
+      setMovies([]);
+    }
+  };
+  return (
+    <div>
+      <Searchbar onSubmit={handleSubmit} />
+      <ul className="">
+        {movies.map((movie) => (
+          <li className="" key={movie.id}>
+            <Link state={location} to={`/movies/${movie.id}`}>
+              {movie.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      {error && <>Sorry. {error} ... 😭</>}
+    </div>
+  );
+};
+export default Movies;
